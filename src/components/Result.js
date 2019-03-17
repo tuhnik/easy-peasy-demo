07c839c141195a454f44a485402014fca8810903
tuhnik <huntlarsen@gmail.com>
2019-03-17 18:29:58 +0200

    ---

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import { useStore } from 'easy-peasy';
 import calculator from '../utils/calculator.js'
-import {Doughnut, Bar, Line} from 'react-chartjs-2';
+import {Doughnut, Bar, Line, Chart} from 'react-chartjs-2';
 
 function Result() {
+
+    Chart.pluginService.register({
+		beforeDraw: function (chart, easing) {
+            var ctx = chart.ctx;
+            var width = chart.width;
+            var height = chart.height;
+            ctx.clearRect(0, 0, width, height)
+            var fontSize = (height / 114).toFixed(2);
+            ctx.font = "bold " +fontSize + "em Verdana";
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = "#4c5b68";
+
+            var percentageText = chart.config.data.percentageText || "",
+                textX = Math.round((width - ctx.measureText(percentageText).width) / 2),
+                textY = height / 2;
+            
+            ctx.fillText(percentageText, textX, textY - 12);
+
+
+            var kwhText = chart.config.data.kwhText || ""
+            ctx.font = (fontSize - 0.3)+ "em Verdana";
+            let kwhTextX = Math.round((width - ctx.measureText(kwhText).width) / 2)
+            ctx.fillText(kwhText, kwhTextX, textY+ 5);
+
+            var unitText = chart.config.data.unitText || ""
+            ctx.font = (fontSize - 0.6)+ "em Verdana";
+            let unitTextX = Math.round((width - ctx.measureText(unitText).width) / 2)
+            ctx.fillText(unitText, unitTextX, textY+ 20);
+
+
+		}
+    });
+    
     const area = useStore(state => state.input.area)
     const buildingType = useStore(state => state.input.buildingType)
     const consumption = useStore(state => state.input.consumption)
@@ -13,13 +46,27 @@ function Result() {
     
     let res = calculator(buildingType, consumption, setupType, area, roofDegrees, orientation)
     
-
-    const data = {
+    
+    const donut1data = {
         labels: ['Oma tarbeks - '+ res.esimeneAasta.omatarbimiseProtsent + '%', 'Müük võrku - '+(100 - res.esimeneAasta.omatarbimiseProtsent)+ '%'],
         
         datasets: [{
             data: [res.esimeneAasta.omatarbimiseProtsent, 100 - res.esimeneAasta.omatarbimiseProtsent],
-            backgroundColor: ['#e89722', '#5cbd4c'] }]
+            backgroundColor: ['#e89722', ] }],
+        percentageText: res.esimeneAasta.omatarbimiseProtsent + "%",
+        kwhText: Math.round(res.esimeneAasta.omaTarbeks),
+        unitText: "kWh"
+    }
+    
+    const donut2data = {
+        labels: ['Oma tarbeks - '+ res.esimeneAasta.omatarbimiseProtsent + '%', 'Müük võrku - '+(100 - res.esimeneAasta.omatarbimiseProtsent)+ '%'],
+        
+        datasets: [{
+            data: [100 - res.esimeneAasta.omatarbimiseProtsent,  res.esimeneAasta.omatarbimiseProtsent],
+            backgroundColor: ['#5cbd4c'] }],
+        percentageText: res.esimeneAasta.müükVõrkuProtsent + "%",
+        kwhText: Math.round(res.esimeneAasta.müükVõrku),
+        unitText: "kWh"
     }
 
     let years = [2019]
@@ -102,14 +149,21 @@ function Result() {
         Sellest oma tarbeks: {Math.round(res.esimeneAasta.omaTarbeks)} kWh ({res.esimeneAasta.omatarbimiseProtsent}%)<Br/>
         Sellest võrku: {Math.round(res.esimeneAasta.müükVõrku)} kWh ({res.esimeneAasta.müükVõrkuProtsent}%)<Br/>
         </div>
-        <div style={{}}>
-            <Doughnut data={data} options={{cutoutPercentage: 60,
-                maintainAspectRatio: false,
+        <div className="donut-container">
+            <span>Oma tarbeks</span>
+            <Doughnut data={donut1data} options={{cutoutPercentage: 70,
                 tooltips: {enabled: false},
-                legend: {position: "right"}
+                legend: {display: false}
             }}/>
-            </div>
-            </div> 
+         </div>
+         <div className="donut-container">
+            <span>Müük võrku</span>
+            <Doughnut data={donut2data} options={{cutoutPercentage: 70,
+                tooltips: {enabled: false},
+                legend: {display: false}
+            }}/>
+         </div>
+        </div> 
        <hr/>
        Maksumus (KM-ga): {Math.round(res.tasuvus.maksumus)} €<Br/>
        Eeldatav sääst 1. aastal: {Math.round(res.tasuvus.säästEsimeselAastal)} €<Br/>
